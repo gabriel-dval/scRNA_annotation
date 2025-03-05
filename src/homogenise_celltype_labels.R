39c6db98305fcdcfc620895db94a2370d59a6c0b
Gabriel Duval <gduvalkieffer@gmail.com>
2025-03-05 11:12:26 +0100

    R script for ct label correction

---
+++ src/homogenise_celltype_labels.R
@@ -0,0 +1,9 @@
+#' The purpose of this script is to test different methods for homogenising
+#' the cell type labels of reference datasets to match the manual annotation 
+#' of the Skull Bone Marrow (SBM) data. This has a number of uses, notably
+#' for comparing the results of different datasets/annotation methods on the
+#' SBM data.
+#' 
+#' NOTE : We will most likely be using the SMB data to match labels across 
+#' datasets - this may cause a bias (overfitting). It is important to remain
+#' cautious about this.
